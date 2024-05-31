790509b92bc054badb37e6bd7c4fe8b77da0e6ef
iWorld-y <caima12138@foxmail.com>
2024-05-31 17:12:00 +0800

    feat: single_linked_list Add

--- lists/single_linked_list.go
+++ lists/single_linked_list.go
@@ -1,25 +1,26 @@
 package lists
 
 type List[T any] struct {
-	first, last *node[T]
+	first, last *Node[T]
 	size        int
+	curr        *Node[T]
 }
-type node[T any] struct {
+type Node[T any] struct {
 	value T
-	next  *node[T]
+	next  *Node[T]
 }
 
 func New[T any](values ...T) *List[T] {
 	list := &List[T]{}
 	if len(values) > 0 {
-		list.Add(values...)
+		list.Append(values...)
 	}
 	return list
 }
 
-func (l *List[T]) Add(values ...T) {
+func (l *List[T]) Append(values ...T) {
 	for _, value := range values {
-		currNode := &node[T]{
+		currNode := &Node[T]{
 			value: value,
 		}
 		if l.size == 0 {
@@ -31,4 +32,44 @@ func (l *List[T]) Add(values ...T) {
 		}
 		l.size++
 	}
+}
+
+func (l *List[T]) InsertHead(values ...T) {
+	if l.size == 0 {
+		l.Append(values...)
+		return
+	}
+	tmpL := New(values...)
+	tmpL.last.next = l.first
+	l.first = tmpL.first
+	l.size += tmpL.size
+}
+
+func (l *List[T]) Iter() *Node[T] {
+	if l.curr == nil {
+		if l.first == nil {
+			return nil
+		}
+		l.curr = l.first
+		return l.curr
+	}
+	if l.curr.next == nil {
+		return nil
+	}
+	l.curr = l.curr.next
+	return l.curr
+}
+
+func (l *List[T]) Size() int {
+	return l.size
+}
+
+func (l *List[T]) Empty() bool {
+	return l.Size() == 0
+}
+
+func (l *List[T]) Clear() {
+	l.first = nil
+	l.last = nil
+	l.size = 0
 }
--- lists/single_linked_list_test.go
+++ lists/single_linked_list_test.go
@@ -2,20 +2,48 @@ package lists
 
 import "testing"
 
-func TestList_Add(t *testing.T) {
-	l := New(1, 3, 5, 7, 9)
-	if l.size != 5 {
-		t.Fatal("New 单链表失败")
+var (
+	testArr    = []int{1, 3, 5, 7, 9}
+	l          = New(testArr...)
+	testArrLen = len(testArr)
+)
+
+func TestList_Append(t *testing.T) {
+	if l.size != len(testArr) {
+		t.Fatal("链表长度错误")
 	}
 	cnt := 0
-	for i := 1; i < 10 && l.first != nil; i += 2 {
-		if l.first.value != i {
-			t.Fatal("New 单链表失败")
+	for _, i := range testArr {
+		curr := l.Iter()
+		if curr == nil {
+			break
 		}
-		l.first = l.first.next
+		if curr.value != i {
+			t.Fatal("链表内元素错误")
+		}
 		cnt++
 	}
 	if cnt != l.size {
-		t.Fatal("New 单链表失败")
+		t.Fatal("未完全遍历链表")
+	}
+}
+func TestList_InsertHead(t *testing.T) {
+	l.InsertHead(testArr...)
+	if l.size != testArrLen*2 {
+		t.Fatal("头插失败")
+	}
+	cnt := 0
+	for _, i := range append(testArr, testArr...) {
+		curr := l.Iter()
+		if curr == nil {
+			break
+		}
+		if curr.value != i {
+			t.Fatal("链表内元素错误")
+		}
+		cnt++
+	}
+	if cnt != l.size {
+		t.Fatal("未完全遍历链表")
 	}
 }
